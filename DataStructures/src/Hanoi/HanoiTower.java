4f079ea60f27a5cb5b9ff76e9c2e3fe52c3791d6
IshiniEdirisinghe <169127269+IshiniEdirisinghe@users.noreply.github.com>
2024-07-01 04:18:44 +0530

    Update HanoiTower.java

--- DataStructures/src/Hanoi/HanoiTower.java
+++ DataStructures/src/Hanoi/HanoiTower.java
@@ -6,10 +6,10 @@ public class HanoiTower {
 		// TODO Auto-generated method stub
 
 		 int n = 3; 
-	        towerOfHanoi(n, 'A', 'C', 'B');
+	        towerOfHanoi(n, "source", "destination", "auxiliary");
 	}
 
-	static void towerOfHanoi(int n, char source, char destination, char auxiliary ) {
+	static void towerOfHanoi(int n, String source, String destination, String auxiliary ) {
 	     if (n == 1) {
 	            System.out.println("Move disk 1 from rod " + source + " to rod " + destination);
 	            return;
